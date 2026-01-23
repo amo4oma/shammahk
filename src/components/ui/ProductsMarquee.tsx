50e1cfeec2e6960ded35afadf9e0a2915bc588d1
Ahmed Obad <63117335+amo4oma@users.noreply.github.com>
2026-01-24 01:10:11 +0300

    sdfsd

--- src/components/ui/ProductsMarquee.tsx
+++ src/components/ui/ProductsMarquee.tsx
@@ -1,21 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { cn } from "@/lib/utils"
 import { Marquee } from "@/components/ui/marquee"
-import qaisar from "../../public/images/qaisar.png"
-import imgs from "../../public/images/ارز-شماخ.png"
-import imgs2 from "../../public/images/ارز-البيت.png"
-import imgs3 from "../../public/images/ارز-الفارس-النبيل.png"
-import imgs4 from "../../public/images/بازلياء-خضراء.png"
-
-import imgs6 from "../../public/images/العطية.png"
-import imgs7 from "../../public/images/التونه.png"
-import imgs8 from "../../public/images/الخريف-موكب-اب.png"
-import imgs9 from "../../public/images/فاصوليا-بيضاء.png"
-import imgs10 from "../../public/images/فاصوليا-حمراء.png"
-import imgs11 from "../../public/images/خيرات-اليمن-موكب-اب.png"
-import leaf from "../../public/images/الشكل-الجمالي-31.png"
-import leaf2 from "../../public/images/الشكل-الجمالي-1-2.png"
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 
 import mobileWheat from "../../public/images/mobile-whest.png"
 import mobileRice from "../../public/images/mobile-rice.png"
@@ -24,22 +10,21 @@ import mobileTuna from "../../public/images/mobile-tuan.png"
 
 const reviews = [
   {
-    name:"whaet",
+    name: "wheat",
     img: mobileWheat,
   },
   {
-    name:"rice",
+    name: "rice",
     img: mobileRice,
   },
   {
-    name:"beans",
+    name: "beans",
     img: mobileBeans,
   },
   {
-    name:"tuna",
+    name: "tuna",
     img: mobileTuna,
   }
-
 ]
 
 const firstRow = reviews.slice(0, reviews.length / 2)
@@ -47,10 +32,8 @@ const secondRow = reviews.slice(reviews.length / 2)
 
 const ReviewCard = ({
   img,
- 
 }: {
-  img: string
- 
+  img: StaticImageData
 }) => {
   return (
     <figure
@@ -64,7 +47,6 @@ const ReviewCard = ({
     >
       <div className="flex flex-row items-center gap-2">
         <Image className="w-full h-full" alt="" src={img} />
- 
       </div>
     </figure>
   )
@@ -74,18 +56,19 @@ export function ProductsMarquee() {
   return (
     <div className="relative flex h-[500px] w-full flex-row items-center justify-center overflow-hidden">
       <Marquee pauseOnHover vertical className="[--duration:20s]">
-        {firstRow.map((review) => (
+        {firstRow.map((review, index) => (
           <ReviewCard key={`first-${review.name}-${index}`} {...review} />
         ))}
       </Marquee>
-        <Marquee pauseOnHover reverse vertical className="[--duration:20s]">
-            {firstRow.map((review) => (
-            <ReviewCard key={`second-${review.name}-${index}`} {...review} />
-            ))}
-        </Marquee>
- 
-      <div className="from-[#abbaab] bg-gradient-to-b pointer-events-none absolute inset-x-0 top-0 h-1/4 "></div>
-      <div className="from-[#ffffff]  bg-gradient-to-t pointer-events-none absolute inset-x-0 bottom-0 h-1/4  "></div>
+      <Marquee pauseOnHover reverse vertical className="[--duration:20s]">
+        {secondRow.map((review, index) => (
+          <ReviewCard key={`second-${review.name}-${index}`} {...review} />
+        ))}
+      </Marquee>
+
+      <div className="from-[#abbaab] bg-linear-to-b pointer-events-none absolute inset-x-0 top-0 h-1/4 "></div>
+      <div className="from-[#ffffff] bg-linear-to-t pointer-events-none absolute inset-x-0 bottom-0 h-1/4 "></div>
     </div>
   )
 }
+
